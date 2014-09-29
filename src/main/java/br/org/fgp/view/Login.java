69ea2f2995e4b0af74bf5287a6c6177d604dc5de
Horácio Dias <horacio.dias92@gmail.com>
2014-09-28 21:18:15 -0300

    Atualizando tela login

--- src/main/java/br/org/fgp/view/Login.java
+++ src/main/java/br/org/fgp/view/Login.java
@@ -58,6 +58,7 @@ public class Login extends JFrame {
 
 	public Login() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setLocationRelativeTo(null);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
